97470d42d25e4ece8e217b1fe080396c8acfebdc
Irina-Piskokha <4526937@gmail.com>
2024-01-18 18:18:29 +0200

    finish

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,18 @@
 import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
 
-// let userSelectedDate = selectedDates[0];
+import 'flatpickr/dist/flatpickr.min.css';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const refs = {
+  dateTimePicker: document.querySelector('#datetime-picker'),
+  btnStart: document.querySelector('[data-start]'),
+  timer: document.querySelector('.timer'),
+};
+
+let intervalId = null;
+let userSelectedDate;
+refs.btnStart.setAttribute('disabled', '');
 
 const options = {
   enableTime: true,
@@ -9,8 +20,72 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const currentDate = new Date();
+    const delta = selectedDates[0] - currentDate;
+    if (delta <= 0) {
+      // window.alert('Please choose a date in the future');
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+        titleColor: 'white',
+        color: '#ef4040',
+        messageColor: 'white',
+      });
+      refs.btnStart.setAttribute('disabled', '');
+      return;
+    } else {
+      refs.btnStart.removeAttribute('disabled');
+      userSelectedDate = selectedDates[0];
+    }
   },
 };
 
-flatpickr('#datetime-picker', options);
+flatpickr(refs.dateTimePicker, options);
+
+refs.btnStart.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+  const intervalId = setInterval(() => {
+    let currentDate = new Date();
+    const ms = userSelectedDate - currentDate;
+    if (ms <= 0) {
+      clearInterval(intervalId);
+      return;
+    }
+
+    refs.btnStart.setAttribute('disabled', '');
+    const date = convertMs(ms);
+    updateTextContent(date);
+  }, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateTextContent(date) {
+  Object.entries(date).forEach(([key, value], index) => {
+    refs.timer.children[index].firstElementChild.textContent =
+      addLeadingZero(value);
+  });
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, 0);
+}
